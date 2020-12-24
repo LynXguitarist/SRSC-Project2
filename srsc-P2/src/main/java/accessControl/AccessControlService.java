93cbc8df9d97caeab4de1bed4448fe038cc39849
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2020-12-24 18:20:38 +0000

    star access

--- srsc-P2/src/main/java/accessControl/AccessControlService.java
+++ srsc-P2/src/main/java/accessControl/AccessControlService.java
@@ -1,5 +1,15 @@
 package accessControl;
 
-public class AccessControlService {
+import api.AccessControl;
+
+public class AccessControlService implements AccessControl {
+
+	@Override
+	public void login(String username, String password) {
+		// gera token
+		// guarda token em session para aquele user
+		// verifica para aquele user as permissoes
+
+	}
 
 }
