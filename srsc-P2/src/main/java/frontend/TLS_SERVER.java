68436f066ec83b424bafe9359c9b3c6e403b4dfb
ricar <ricar@LAPTOP-G8F6HP7J>
2020-12-29 20:09:03 +0000

    tls com erros

---
+++ srsc-P2/src/main/java/frontend/TLS_SERVER.java
@@ -0,0 +1,106 @@
+package frontend;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.security.KeyStore;
+import java.util.Properties;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class TLS_SERVER {
+
+	public static void main(String[] args) throws Exception {
+		String host = null;
+		int port = -1;
+		String path = null;
+		for (int i = 0; i < args.length; i++)
+			System.out.println(args[i]);
+
+		if (args.length < 3) {
+			System.out.println("USAGE: java SSLSocketClientWithClientAuth " + "host port requestedfilepath");
+			System.exit(-1);
+		}
+
+		try {
+			host = args[0];
+			port = Integer.parseInt(args[1]);
+			path = args[2];
+		} catch (IllegalArgumentException e) {
+			System.out.println("USAGE: java SSLSocketClientWithClientAuth " + "host port requestedfilepath");
+			System.exit(-1);
+		}
+
+		try {
+
+			/*
+			 * Set up a key manager for client authentication if asked by the server. Use
+			 * the implementation's default TrustStore and secureRandom routines.
+			 */
+			SSLSocketFactory factory = null;
+			try {
+				SSLContext ctx;
+				KeyManagerFactory kmf;
+				KeyStore ks;
+				char[] passphrase = "clientsrsc".toCharArray();
+
+				ctx = SSLContext.getInstance("TLS");
+				kmf = KeyManagerFactory.getInstance("SunX509");
+				ks = KeyStore.getInstance("JKS");
+				
+				ks.load(new FileInputStream("trustedstore"), passphrase);
+				
+				kmf.init(ks, passphrase);
+				ctx.init(kmf.getKeyManagers(), null, null);
+
+				factory = ctx.getSocketFactory();
+			} catch (Exception e) {
+				throw new IOException(e.getMessage());
+			}
+
+			SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+
+			/*
+			 * send http request
+			 *
+			 * See SSLSocketClient.java for more information about why there is a forced
+			 * handshake here when using PrintWriters.
+			 */
+			socket.startHandshake();
+
+			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+			out.println("GET " + path + " HTTP/1.0");
+			out.println();
+			out.flush();
+
+			/*
+			 * Make sure there were no surprises
+			 */
+			if (out.checkError())
+				System.out.println("SSLSocketClient: java.io.PrintWriter error");
+
+			/* read response */
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+
+			in.close();
+			out.close();
+			socket.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
